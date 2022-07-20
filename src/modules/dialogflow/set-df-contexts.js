6ee6d7739adc5ab81f49039a11f7f173c2fcaa13
Alan Martines <alancpmartines@hotmail.com>
2022-07-20 08:32:57 -0400

    Update set-df-contexts.js
    Add get parameter Dialogflow

--- src/modules/dialogflow/set-df-contexts.js
+++ src/modules/dialogflow/set-df-contexts.js
@@ -1,7 +1,27 @@
 const log = require('@logger')
 const dialogflow = require('@google-cloud/dialogflow')
 const { jsonParse } = require('@helpers')
-const CREDENTIALS = jsonParse(process.env.GCLOUD_CREDENTIALS)
+
+// Checa possíveis erros
+const CREDENTIAL = '{
+  "type": process.env.TYPE,
+  "project_id": process.env.PROJECT_ID,
+  "private_key_id": process.env.PRIVATE_KEY_ID,
+  "private_key": process.env.PRIVATE_KEY,
+  "client_email": process.env.CLIENT_EMAIL,
+  "client_id": process.env.CLIENT_ID,
+  "auth_uri": process.env.AUTH_URI,
+  "token_uri": process.env.TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
+}';
+try {
+	const CREDENTIALS = jsonParse(CREDENTIAL)
+} catch {
+	error = true
+	log('redBright', 'Erro')('Credenciais do Dialogflow faltando')
+	log('magentaBright', 'Erro')('Inclua suas credenciais do Dialogflow na variável de ambiente GCLOUD_CREDENTIALS')
+}
 
 /**
  * Atualiza os contextos expirados no Dialogflow
